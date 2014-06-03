f462af3278fb4578d5053ea47a96ae981295be73
martj <martj@mail.bg>
2014-06-03 17:49:43 +0300

    Format the code

--- Project1/src/com/hackbulgaria/corejava/Problems2Impl.java
+++ Project1/src/com/hackbulgaria/corejava/Problems2Impl.java
@@ -10,7 +10,7 @@ import org.omg.CORBA.OMGVMCID;
 import org.omg.CORBA.portable.ValueOutputStream;
 
 public class Problems2Impl implements Problems2 {
-
+    /* ................................................................ */
     @Override
     public boolean isOdd(int number) {
         if (!(number % 2 == 0)) {
@@ -19,9 +19,7 @@ public class Problems2Impl implements Problems2 {
         return false;
     }
 
-    /*
-     * ...........................................................................
-     */
+    /* ................................................................ */
     @Override
     public boolean isPrime(int number) {
         if (number == 1)
@@ -34,10 +32,7 @@ public class Problems2Impl implements Problems2 {
         return true;
     }
 
-    /*
-     * ...........................................................................
-     */
-    @Override
+    /* ................................................................ */@Override
     public int min(int... array) {
         int minElem;
         minElem = array[0];
@@ -50,9 +45,7 @@ public class Problems2Impl implements Problems2 {
         // return 0;
     }
 
-    /*
-     * ...........................................................................
-     */
+    /* ................................................................ */
     @Override
     public int kthMin(int k, int[] array) {
         int minElem;
@@ -73,9 +66,7 @@ public class Problems2Impl implements Problems2 {
         return array[k];
     }
 
-    /*
-     * ...........................................................................
-     */
+    /* ................................................................ */
     @Override
     public float getAverage(int[] array) {
         float sum = 0;
@@ -86,9 +77,7 @@ public class Problems2Impl implements Problems2 {
         return sum / array.length;
     }
 
-    /*
-     * ...........................................................................
-     */
+    /* ................................................................ */
     @Override
     public long getSmallestMultiple(int upperBound) {
         // TODO Auto-generated method stub
@@ -96,17 +85,18 @@ public class Problems2Impl implements Problems2 {
         return 0;
     }
 
+    /* ................................................................ */
     public boolean isPalindrome(long n) {
-//        ArrayList<Object> myList = new ArrayList<>();
-//        while (n >0){
-//            myList.add(n%10);
-//            n= n/10;
-//        }
-//        String temp = myList.toString();
+        // ArrayList<Object> myList = new ArrayList<>();
+        // while (n >0){
+        // myList.add(n%10);
+        // n= n/10;
+        // }
+        // String temp = myList.toString();
         String temp = String.valueOf(n);
         System.out.println(temp);
-        int k = temp.length()-1;
-        for (int i = 0; i < temp.length()-1; i++) {
+        int k = temp.length() - 1;
+        for (int i = 0; i < temp.length() - 1; i++) {
             if (temp.charAt(i) != temp.charAt(k)) {
                 return false;
             }
@@ -115,14 +105,17 @@ public class Problems2Impl implements Problems2 {
         return true;
     }
 
+    /* ................................................................ */
     @Override
     public long getLargestPalindrome(long N) {
-        if(isPalindrome(N-1)){
-            return N-1;
-        }else getLargestPalindrome(N-1);
+        if (isPalindrome(N - 1)) {
+            return N - 1;
+        } else
+            getLargestPalindrome(N - 1);
         return 0;
     }
 
+    /* ................................................................ */
     @Override
     public int[] histogram(short[][] image) {
         // TODO Auto-generated method stub
@@ -137,6 +130,7 @@ public class Problems2Impl implements Problems2 {
         return fact;
     }
 
+    /* ................................................................ */
     @Override
     public long doubleFac(int n) {
 
@@ -147,6 +141,7 @@ public class Problems2Impl implements Problems2 {
         return fact;
     }
 
+    /* ................................................................ */
     @Override
     public long kthFac(int k, int n) {
         // TODO Auto-generated method stub
@@ -157,25 +152,28 @@ public class Problems2Impl implements Problems2 {
         return fact;
     }
 
+    /* ................................................................ */
     @Override
     public int getOddOccurrence(int[] array) {
-        //(1,1,2,2,3,3,4,4,2) -> 2
+        // (1,1,2,2,3,3,4,4,2) -> 2
         Arrays.sort(array);
-        if(array[0] != array[1]){
+        if (array[0] != array[1]) {
             return array[0];
         }
         int count = 0;
         for (int i = 1; i < array.length; i++) {
-            if(array[i] == array[i-1]){
-                count ++;
-            }else if(count % 2 == 0){
-                return array[i-1];
-            }else count = 0;
+            if (array[i] == array[i - 1]) {
+                count++;
+            } else if (count % 2 == 0) {
+                return array[i - 1];
+            } else
+                count = 0;
         }
         // TODO Auto-generated method stub
         return 0;
     }
 
+    /* ................................................................ */
     @Override
     public long pow(int a, int b) {
         // TODO Auto-generated method stub
@@ -186,6 +184,7 @@ public class Problems2Impl implements Problems2 {
         return tmp;
     }
 
+    /* ................................................................ */
     @Override
     public long maximalScalarSum(int[] a, int[] b) {
         long scalarSum = 0;
@@ -198,37 +197,41 @@ public class Problems2Impl implements Problems2 {
         return scalarSum;
     }
 
+    /* ................................................................ */
     @Override
     public int maxSpan(int[] array) {
-        
+
         // TODO Auto-generated method stub
         return 0;
     }
 
+    /* ................................................................ */
     @Override
     public boolean canBalance(int[] array) {
-        for (int i = 0 ; i < array.length; i ++){
+        for (int i = 0; i < array.length; i++) {
             int sum1 = 0;
             int sum2 = 0;
-            for (int j = 0; j<=i;j++){
+            for (int j = 0; j <= i; j++) {
                 sum1 += array[j];
             }
-            for (int j = array.length-1; j>i;j--){
+            for (int j = array.length - 1; j > i; j--) {
                 sum2 += array[j];
             }
-            if(sum1 == sum2)
+            if (sum1 == sum2)
                 return true;
         }
         // TODO Auto-generated method stub
         return false;
     }
 
+    /* ................................................................ */
     @Override
     public int[][] rescale(int[][] original, int newWidth, int newHeight) {
         // TODO Auto-generated method stub
         return null;
     }
 
+    /* ................................................................ */
     @Override
     public String reverseMe(String argument) {
         // TODO Auto-generated method stub
@@ -239,6 +242,7 @@ public class Problems2Impl implements Problems2 {
         return temp.toString();
     }
 
+    /* ................................................................ */
     @Override
     public String copyEveryChar(String input, int k) {
         StringBuilder temp = new StringBuilder();
@@ -250,6 +254,7 @@ public class Problems2Impl implements Problems2 {
         return temp.toString();
     }
 
+    /* ................................................................ */
     @Override
     public String reverseEveryWord(String arg) {
         String[] words = arg.split(" ");
@@ -266,6 +271,7 @@ public class Problems2Impl implements Problems2 {
         return temp.toString();
     }
 
+    /* ................................................................ */
     @Override
     public boolean isPalindrome(String argument) {
 
@@ -279,11 +285,12 @@ public class Problems2Impl implements Problems2 {
         return true;
     }
 
+    /* ................................................................ */
     @Override
     public boolean isPalindrome(int number) {
         String temp = String.valueOf(number);
-        int k = temp.length()-1;
-        for (int i = 0; i < temp.length()-1; i++) {
+        int k = temp.length() - 1;
+        for (int i = 0; i < temp.length() - 1; i++) {
             if (temp.charAt(i) != temp.charAt(k)) {
                 return false;
             }
@@ -292,49 +299,56 @@ public class Problems2Impl implements Problems2 {
         return true;
     }
 
+    /* ................................................................ */
     @Override
     public int getPalindromeLength(String input) {
-        for(int i = 0; i < input.length()/2; i++){
-            if(i == input.length()-1 - i){
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (i == input.length() - 1 - i) {
                 return 0;
             }
-            if(input.charAt(i) == input.charAt(input.length()-1 - i)){
-                if(isPalindrome(input.substring(i, input.length()-i))){
-                    return (input.length()-1-2*i)/2;
+            if (input.charAt(i) == input.charAt(input.length() - 1 - i)) {
+                if (isPalindrome(input.substring(i, input.length() - i))) {
+                    return (input.length() - 1 - 2 * i) / 2;
                 }
-            }            
+            }
         }
         // TODO Auto-generated method stub
         return 0;
     }
 
+    /* ................................................................ */
     @Override
     public int countOcurrences(String needle, String haystack) {
+        // kolko puti shte namer needle v haystack
+        haystack.contains(needle);
         // TODO Auto-generated method stub
         return 0;
     }
 
+    /* ................................................................ */
     @Override
     public String decodeURL(String input) {
-       String temp = input.replace("%2F", "/");
+        String temp = input.replace("%2F", "/");
         // TODO Auto-generated method stub
         return temp.replace("%3A", ":");
     }
 
+    /* ................................................................ */
     @Override
     public int sumOfNumbers(String input) {
         String[] splited = input.split("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)|( )");
         for (String string : splited) {
-           if(string.contains()){
-               
-           }
-           
+            if (string.contains()) {
+
+            }
+
             System.out.println(string);
         }
         // TODO Auto-generated method stub
         return 999;
     }
 
+    /* ................................................................ */
     @Override
     public boolean areAnagrams(String A, String B) {
         // TODO Auto-generated method stub
@@ -360,9 +374,9 @@ public class Problems2Impl implements Problems2 {
         char[] charsA = aTmp.toCharArray();
         char[] charsB = bTmp.toCharArray();
         Arrays.sort(charsA);
-       
+
         for (int i = 0; i < charsB.length; i++) {
-        
+
             if (charsB[i] == charsA[0]) {
                 char[] temp = Arrays.copyOfRange(charsB, i, i + charsA.length);
                 Arrays.sort(temp);
@@ -370,21 +384,23 @@ public class Problems2Impl implements Problems2 {
                     if (temp[j] != charsA[j]) {
                         break;
                     }
-                } if (temp[charsA.length-1] == charsA[charsA.length-1])
+                }
+                if (temp[charsA.length - 1] == charsA[charsA.length - 1])
                     return true;
                 if (i < charsA.length - 1)
                     continue;
-                char[] tempR = Arrays.copyOfRange(charsB, i - charsA.length + 1, i+1);
+                char[] tempR = Arrays.copyOfRange(charsB, i - charsA.length + 1, i + 1);
                 Arrays.sort(tempR);
                 for (int j = 0; j < charsA.length; j++) {
                     if (tempR[j] != charsA[j]) {
                         break;
                     }
-                }if (tempR[charsA.length-1] == charsA[charsA.length-1])
+                }
+                if (tempR[charsA.length - 1] == charsA[charsA.length - 1])
                     return true;
                 continue;
             }// close A == B
-            
+
         }// close if
 
         return false;
